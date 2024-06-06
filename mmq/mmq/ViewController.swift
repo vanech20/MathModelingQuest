9c32b1296819684ca4c817411c327bd80b6014e6
vanech20 <estefyvane2007@gmail.com>
2024-06-06 12:41:04 -0600

    Initial commit
    Main page and register page

---
+++ mmq/mmq/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mmq
+//
+//  Created by Desarrollo on 06/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
